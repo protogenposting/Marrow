548412b4a98eda3b70e14cdacfebc05fc674646f
protogenposting <86360309+protogenposting@users.noreply.github.com>
2024-08-26 12:45:42 -0400

    Update Game1.cs

--- Marrow/Marrow/Game1.cs
+++ Marrow/Marrow/Game1.cs
@@ -18,16 +18,12 @@ public class Game1 : Game
 
     protected override void Initialize()
     {
-        // TODO: Add your initialization logic here
-
         base.Initialize();
     }
 
     protected override void LoadContent()
     {
         _spriteBatch = new SpriteBatch(GraphicsDevice);
-
-        // TODO: use this.Content to load your game content here
     }
 
     protected override void Update(GameTime gameTime)
@@ -35,16 +31,12 @@ public class Game1 : Game
         if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
             Exit();
 
-        // TODO: Add your update logic here
-
         base.Update(gameTime);
     }
 
     protected override void Draw(GameTime gameTime)
     {
         GraphicsDevice.Clear(Color.Azure);
-
-        // TODO: Add your drawing code here
 
         base.Draw(gameTime);
     }
