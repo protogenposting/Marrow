11c9792967c14c02a9fae32ae49d1a8994f78b7c
protogenposting <86360309+protogenposting@users.noreply.github.com>
2024-08-26 12:42:00 -0400

    gay

--- Marrow/Marrow/Game1.cs
+++ Marrow/Marrow/Game1.cs
@@ -42,7 +42,7 @@ public class Game1 : Game
 
     protected override void Draw(GameTime gameTime)
     {
-        GraphicsDevice.Clear(Color.CornflowerBlue);
+        GraphicsDevice.Clear(Color.Azure);
 
         // TODO: Add your drawing code here
 
