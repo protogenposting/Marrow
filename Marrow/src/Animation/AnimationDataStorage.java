cc4698932d2fb463f227f3e37983f5f950cbbce7
protogenposting <86360309+protogenposting@users.noreply.github.com>
2024-10-14 11:44:49 -0400

    Update AnimationDataStorage.java

--- Marrow/src/Animation/AnimationDataStorage.java
+++ Marrow/src/Animation/AnimationDataStorage.java
@@ -1,8 +1,11 @@
 package Animation;
 
+import java.util.ArrayList;
+
 public class AnimationDataStorage {
     public int currentFrame = 0;
     public boolean isInAnimateMode = false;
     public boolean isPlaying = false;
     public int keyFrameID = 0;
+    public ArrayList<Keyframe> keyframes = new ArrayList<>();
 }
