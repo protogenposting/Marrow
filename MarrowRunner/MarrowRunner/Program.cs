0c5381b218e4324891062f598a7ce601d0f020d6
protogenposting <86360309+protogenposting@users.noreply.github.com>
2024-10-28 15:06:13 -0400

    removed bad crap

--- MarrowRunner/MarrowRunner/Program.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Diagnostics;
-
-Process process = new Process();
-
-process.StartInfo.FileName = "java";
-
-process.StartInfo.Arguments = "-jar "+System.IO.Path.GetDirectoryName(System.Reflection.Assembly.GetEntryAssembly().Location)+"\\Marrow.jar";
-
-process.Start();
--- README.md
+++
@@ -1,5 +0,0 @@
-Marrow is an open source skeleton based animation program!
-
-Thanks to gabrien for the icon!
-
-join our discord: https://discord.gg/k64EDNHsrj
